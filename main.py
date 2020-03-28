af9171d1f75a8be4069411187a584ad541a451e3
Akash97p <akash.anjanam@gmail.com>
2020-03-28 15:01:40 +0530

    Removed Threading

--- main.py
+++ main.py
@@ -1,24 +1,15 @@
 from dependency import *
-global exit_flag
-exit_flag = False
+
 #----------------Titile ASCII Text with arts fonts--------------
 
 title_art('Serial CLI')
 
 #-------------------------main_menu-----------------------------
-
+while True:
+    mm()
 #------------------------Config_Selection-----------------------
 
 #---------------------------Serial_Listener---------------------
 
-# main Threads
-menu = threading.Thread(target=mm)
-update_config = threading.Thread(target=prev_update)
-menu.start()
-update_config.start()
 
-while exit_flag :
-    menu.stop()
-    update_config.stop()
-    exit()
 
